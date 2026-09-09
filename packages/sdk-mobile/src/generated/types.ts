1cc0b89aab83feecf9958490c5056d0ec9a902ad
Yazan Daoud <anxiouscrypt@gmail.com>
2026-09-09 11:10:09 -0400

    chore: regenerate reporting API artifacts

--- packages/sdk-mobile/src/generated/types.ts
+++ packages/sdk-mobile/src/generated/types.ts
@@ -1953,6 +1953,39 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/admin/reporting/query": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get?: never;
+        put?: never;
+        post: {
+            parameters: {
+                query?: never;
+                header?: never;
+                path?: never;
+                cookie?: never;
+            };
+            requestBody?: never;
+            responses: {
+                /** @description Default Response */
+                200: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content?: never;
+                };
+            };
+        };
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
     "/admin/orders": {
         parameters: {
             query?: never;
